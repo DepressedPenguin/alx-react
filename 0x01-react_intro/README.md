c56efaa8755676972d35b44b7d49905d1956176f
ZAKARIA <zakariaelaro@gmail.com>
2024-01-30 16:09:36 +0000

    Check task 0

---
+++ 0x01-react_intro/README.md
@@ -0,0 +1,1 @@
+<h1>0x01-react_intro</h1>
---
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import logo_hol from './logo_hol.jpg'
+import './App.css'
+// IMPORT TWO FUNCTIONS
+import {getFullYear,getFooterCopy} from './utils'
+function App() {
+  return (
+    <div className="App">
+      {/* HEADER */}
+      <header className='App-header'>
+        <img src={logo_hol} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </header>
+      {/* BODY */}
+      <body className='App-body'>
+        <p>Login to access the full dashboard</p>
+      </body>
+      {/* FOOTER */}
+      <footer className='App-footer'>
+        <p>Copyright 2024 - holberton School</p>
+        <getFullYear/>
+      </footer>
+    </div>
+
+  );
+}
+
+export default App;
