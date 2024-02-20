76d2e11d3cbd2f7fe263643ae659bc1718853064
ZAKARIA <zakariaelaro@gmail.com>
2024-02-20 10:22:50 +0000

    Task 0 fix

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,6 +1,17 @@
-import { Map } from 'immutable';
-
+const { Map } = require('immutable');
 // CREATE GET IMMUTABLEOBJECT
 function getImmutableObject(object){
     return Map(object);
 }
+
+// CREATE OBJECT
+const my_object = {
+    fear: true,
+    smell: -1033575916.9145899,
+    wall: false,
+    thing: -914767132
+}
+
+// DISPLAY THE RESULT
+const immutableMap = getImmutableObject(my_object);
+console.log(immutableMap);
